6078083500bf32bacb1164bccb02fdb2c1312f4f
asacraft <asakura.craft@gmail.com>
2012-05-10 10:05:53 +0900

    problem 3

--- src/euler/core.clj
+++ src/euler/core.clj
@@ -1,4 +1,5 @@
-(ns euler.core)
+(ns euler.core
+  (:use clojure.contrib.math))
 
 (defn multiple? [n m]
   (zero? (rem n m)))
@@ -6,13 +7,18 @@
 (defn p001 [m]
   (reduce + (filter #(or (multiple? % 3) (multiple? % 5)) (range m))))
 
-(defn fib [a b]
-  (let [n (+ a b)]
-    (lazy-seq (cons n (fib b n)))))
+(defn fib []
+  (map first (iterate (fn [[a b]] [b (+ a b)]) [0 1])))
 
 (defn p002 [m]
-  (reduce + (filter even? (take-while #(< % m) (fib 0 1)))))
+  (reduce + (filter even? (take-while #(< % m) (fib)))))
+
+(defn prime? [n]
+  (empty? (drop-while #(not (multiple? n %)) (range 2 (inc (/ n 2))))))
+
+(defn p003 [m]
+  (first (filter #(and (multiple? m %) (prime? %)) (iterate dec (int (sqrt m))))))
 
 (defn -main []
-  (p002 4000000))
+  (p003 600851475143))
 
--- test/euler/test/core.clj
+++ test/euler/test/core.clj
@@ -5,3 +5,9 @@
 (deftest test-p001
   (is (= 23 (p001 10))))
 
+(deftest test-p002
+  (is (= 10 (p002 20))))
+
+(deftest test-p003
+  (is (= 29 (p003 13195))))
+
